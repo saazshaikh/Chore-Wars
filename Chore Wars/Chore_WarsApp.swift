2e4c1a1b8f231b0ed64acb1f70c83c93b3a95b13
Saaz S <saazshaikh.edu@gmail.com>
2023-07-27 07:44:04 +0100

    Initial Commit

---
+++ Chore Wars/Chore_WarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chore_WarsApp.swift
+//  Chore Wars
+//
+//  Created by Saaz Shaikh on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Chore_WarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
